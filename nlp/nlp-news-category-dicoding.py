e784212321cf8d8034efad40d2eb81d29e45c9df
Picessa Kresna <kresna.picessa@gmail.com>
2024-01-01 21:15:34 +0800

    add

---
+++ nlp/nlp-news-category-dicoding.py
@@ -0,0 +1,160 @@
+import csv
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
+
+import requests
+url = "https://github.com/picessakresna/data-portofolio/raw/f0021c54ac6f2a184e8cddb9a88bc7e05b900a8d/nlp/dataset/bbc-text.csv"
+filename = "bbc-text.csv"
+
+response = requests.get(url)
+with open(filename, 'wb') as file:
+    file.write(response.content)
+
+with open(filename, 'r') as csvfile:
+    print(f"First line (header) looks like this:\n\n{csvfile.readline()}")
+    print(f"Each data point looks like this:\n\n{csvfile.readline()}")
+
+NUM_WORDS = 1000
+EMBEDDING_DIM = 16
+MAXLEN = 120
+PADDING = 'post'
+OOV_TOKEN = "<OOV>"
+TRAINING_SPLIT = .8
+
+
+def remove_stopwords(sentence):
+    stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+    
+    sentence = sentence.lower()
+
+    words = sentence.split()
+    no_words = [w for w in words if w not in stopwords]
+    sentence = " ".join(no_words)
+
+    return sentence
+
+
+def parse_data_from_file(filename):
+
+    sentences = []
+    labels = []
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            labels.append(row[0])
+            sentence = row[1]
+            sentence = remove_stopwords(sentence)
+            sentences.append(sentence)
+
+    return sentences, labels
+
+sentences, labels = parse_data_from_file("bbc-text.csv")
+
+print(f"There are {len(sentences)} sentences in the dataset.\n")
+print(f"First sentence has {len(sentences[0].split())} words (after removing stopwords).\n")
+print(f"There are {len(labels)} labels in the dataset.\n")
+print(f"The first 5 labels are {labels[:5]}")
+
+def train_val_split(sentences, labels, training_split):
+
+    train_size = int(len(sentences)*training_split)
+
+    train_sentences = sentences[:train_size]
+    train_labels = labels[:train_size]
+
+    validation_sentences = sentences[train_size:]
+    validation_labels = labels[train_size:]
+    
+    return train_sentences, validation_sentences, train_labels, validation_labels
+
+train_sentences, val_sentences, train_labels, val_labels = train_val_split(sentences, labels, TRAINING_SPLIT)
+
+print(f"There are {len(train_sentences)} sentences for training.\n")
+print(f"There are {len(train_labels)} labels for training.\n")
+print(f"There are {len(val_sentences)} sentences for validation.\n")
+print(f"There are {len(val_labels)} labels for validation.")
+
+def fit_tokenizer(train_sentences, num_words, oov_token):
+
+    tokenizer = Tokenizer(num_words = num_words, oov_token = oov_token)
+
+    tokenizer.fit_on_texts(train_sentences)
+
+    return tokenizer
+
+tokenizer = fit_tokenizer(train_sentences, NUM_WORDS, OOV_TOKEN)
+word_index = tokenizer.word_index
+
+print(f"Vocabulary contains {len(word_index)} words\n")
+print("<OOV> token included in vocabulary" if "<OOV>" in word_index else "<OOV> token NOT included in vocabulary")
+
+def seq_and_pad(sentences, tokenizer, padding, maxlen):
+  
+    sequences = tokenizer.texts_to_sequences(sentences)
+    
+    padded_sequences = pad_sequences(sequences, maxlen=maxlen, padding=padding)
+
+    return padded_sequences
+
+train_padded_seq = seq_and_pad(train_sentences, tokenizer, PADDING, MAXLEN)
+val_padded_seq = seq_and_pad(val_sentences, tokenizer, PADDING, MAXLEN)
+
+print(f"Padded training sequences have shape: {train_padded_seq.shape}\n")
+print(f"Padded validation sequences have shape: {val_padded_seq.shape}")
+
+def tokenize_labels(all_labels, split_labels):
+
+    label_tokenizer = Tokenizer()
+
+    label_tokenizer.fit_on_texts(all_labels)
+
+    label_seq = label_tokenizer.texts_to_sequences(split_labels)
+
+    label_seq_np = np.array(label_seq) - 1
+
+    return label_seq_np
+
+train_label_seq = tokenize_labels(labels, train_labels)
+val_label_seq = tokenize_labels(labels, val_labels)
+
+print(f"First 5 labels of the training set should look like this:\n{train_label_seq[:5]}\n")
+print(f"First 5 labels of the validation set should look like this:\n{val_label_seq[:5]}\n")
+print(f"Tokenized labels of the training set have shape: {train_label_seq.shape}\n")
+print(f"Tokenized labels of the validation set have shape: {val_label_seq.shape}\n")
+
+def create_model(num_words, embedding_dim, maxlen):
+
+    tf.random.set_seed(123)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(num_words, embedding_dim, input_length=maxlen),
+        tf.keras.layers.LSTM(64),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(5, activation='softmax')
+    ])
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+model = create_model(NUM_WORDS, EMBEDDING_DIM, MAXLEN)
+
+history = model.fit(train_padded_seq, train_label_seq, epochs=30, validation_data=(val_padded_seq, val_label_seq))
+
+def plot_graphs(history, metric):
+    plt.plot(history.history[metric])
+    plt.plot(history.history[f'val_{metric}'])
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend([metric, f'val_{metric}'])
+    plt.show()
+    
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
